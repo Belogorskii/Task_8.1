a03c5e16730fead20b09280b6839092f202e1169
Kirill Miller <belogorski2000@gmail.com>
2022-11-18 16:43:28 +0300

    Добавьте файлы проекта.

---
+++ Task_5.1/Main.cs
@@ -0,0 +1,109 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+
+namespace Task_6._1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute (ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Documet doc = commandData.Application.ActiveUIDocument.Document;
+            using (var ts = new Transaction(doc, "export ifs"))
+            {
+                ts.Start();
+
+                ViewPlan viewPlan = new FilteredElementCollector(doc)
+                    .OfClass(typeof(ViewPlan))
+                    .Cast<viewPlan>()
+                    FirstOrDefault(viewPlan => v.ViewType.FloorPlan &&
+                    v.Name.Equals("Level 1"));
+                var ifsOption = new IFSExportOptions();
+                doc.Export(Enviroment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.ifs",
+                    new List<ElementId> { viewPlan.Id}, ifsOption;
+                ts.Commit();
+            }
+            return Result.Succeeded;
+        }
+        public void BatchPrint(DocumentPage doc)
+        {
+            var sheets = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(ViewSheet))
+                .Cast<ViewSheet>()
+                .ToList();
+            var groupedSheets = sheets.GroupBy(sheets => doc.GetElement(new FilteredElementCollector(doc, sheets.Id)
+                .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                .FirstElementId()).Name);
+            var viewSets = new List<ViewSet>();
+            PrintManager printManager = doc.PrintManager;
+            printManager.SelectNewPrintDriver("PDFCeator");
+            ViewSheetSetting = printManager.ViewSheetSetting;
+            foreach (var groupedSheet in groupedSheets)
+            {
+                if (groupedSheet.Key == null)
+                    continue;
+                var view Set = new ViewSet();
+                var sheetsOfGroup = groupedSheet.Select(sbyte => s).ToList();
+                foreach (var sheet in sheetsOfGroup)
+                {
+                    viewSet.Insert(sheet);
+                }
+                viewSets.Add(viewSet);
+           
+                printManager.PrintRange = PrintRange.Select;
+                viewSheetSetting.CurrentViewSheetSet.Views = viewSet;
+
+                using (var ts = new Transaction(doc, "Create view set"))
+                {
+                     ts.Start();
+                     viewSheetSetting.SaveAs($"{groupedSheet.Key}_{Guid.NewGuid()}");
+                     ts.Commit();
+                }
+
+                bool isFormatSelected = false;
+                foreach (PaperSize paperSize in printManager.PaperSizes)
+                {
+                    if(string.Equals(groupedSheet.Key, "A4K") &&
+                      string.Equals(paperSise.Name, "A4"))
+                    {
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PaperSize = paperSize;
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PageOrientation = PageOrientationType.Portrait;
+                        isFormatSelected = true;
+                    }
+                    else if(string.Equals(groupedSheet.Key, "A3A") &&
+                      string.Equals(paperSise.Name, "A3 "))
+                    {
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PaperSize = paperSize;
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PageOrientation = PageOrientationType.Landscape;
+                        isFormatSelected = true;
+                    }
+                }
+                if(!isPormatSelected)
+                {
+                     TaskDialog.Show("Ошибка", "Не найден формат")
+                     return Result.Failed;
+                }
+                printManager.CombinedFile = false;
+                printManager.SubmitPrint();
+            }
+        }
+    }
+}
---
+++ Task_5.1/MainViewViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Task_5._1
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public Furniture Furniture { get; }
+
+        public List<FamilySymbol> FamilyTypes { get; } = new List<FamilySymbol>();
+
+        public DelegateCommand SaveCommand { get; }
+
+        public FamilySymbol SelectedFamilyType { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+
+         {
+            _commandData = commandData;
+            Furniture = SelectionUtils.GetObject<Furniture>(commandData, "Выберете мебель");
+            FamilyTypes = FamilySymbolUtils.GetFamilySymbols(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+         }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var locationCurve = Furniture.Location as locationCurve;
+            var pipeCurve = locationCurve.Curve;
+            var oLevel = (Level)doc.GetElement(Furniture.LevelId);
+
+            FamilyInstanceUtills.CreateFamilyInstance(_commandData, SelectedFamilyType, furnitureCurve.GetEndPoint(0), oLevel);
+            FamilyInstanceUtills.CreateFamilyInstance(_commandData, SelectedFamilyType, furnitureCurve.GetEndPoint(1), oLevel);
+
+            RaiseCloseRequest();
+
+        }
+        
+        public event EventHandler CloseRequested;
+        private void RaiseCloseRequest()
+        { 
+            CloseRequested?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
